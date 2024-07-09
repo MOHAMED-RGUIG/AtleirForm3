5d122ef304517c02d39557ffbf82f7780437a7b8
MOHAMED-RGUIG <135667611+MOHAMED-RGUIG@users.noreply.github.com>
2024-07-09 17:53:56 +0200

    Update Pcescreen.js

--- src/screens/Pcescreen.js
+++ src/screens/Pcescreen.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getDem } from '../actions/pceActions';
-
 import AddPiecePopup from '../components/AddPiecePopup';
+
 function Pcescreen() {
     const [_idDem, set_idDem] = useState('');
     const [clt, setclt] = useState('');
@@ -10,6 +10,7 @@ function Pcescreen() {
     const [NSerie, setNSerie] = useState('');
     const [pieces, setPieces] = useState([]);
     const [showPopup, setShowPopup] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('DEM');
 
     const dispatch = useDispatch();
     const demState = useSelector(state => state.getDemReducer);
@@ -43,69 +44,111 @@ function Pcescreen() {
         setShowPopup(true);
     };
 
+    const handleDeletePiece = (index) => {
+        const newPieces = pieces.filter((_, i) => i !== index);
+        setPieces(newPieces);
+    };
+
+    const filteredData = data.filter(item => item._idDem.toLowerCase().includes(searchTerm.toLowerCase()));
+
+    const handleSearchTermChange = (e) => {
+        const value = e.target.value.toUpperCase();
+        if (value.startsWith('DEM')) {
+            setSearchTerm(value);
+        } else {
+            setSearchTerm('DEM');
+        }
+    };
+
     return (
         <div>
             <div className='row justify-content-center'> 
-         
-            <div className="position-relative">
-    <img src="../black-coffee-surrounded-by-coffee-beans.webp" alt="Lavazza" className="img-fluid w-100 col-11 col-md-11" style={{height:'350px'}} />
-  
-</div>  
-
-  
+                <div className="position-relative">
+                    <img src="../black-coffee-surrounded-by-coffee-beans.webp" alt="Lavazza" className="img-fluid w-100 col-11 col-md-11" style={{height:'350px'}} />
+                </div>  
                 <div className='col-md-11 col-11 text-start shadow-lg p-3 mb-5 mt-3 bg-body rounded'>
-                  
                     <div>
-                        <input required type='text' placeholder='Entrer le n° de la demande service' className='form-control'
-                            value={_idDem} onChange={(e) => { set_idDem(e.target.value) }}
+                        <input 
+                            required 
+                            type='text' 
+                            placeholder='Recherche par n° de la demande service' 
+                            className='form-control mb-2'
+                            value={searchTerm} 
+                            onChange={handleSearchTermChange}
                         />
-                        <input required type='text' placeholder='Code client' className='form-control'
-                            value={clt} disabled onChange={(e) => { setclt(e.target.value) }}
+                        <select 
+                            required 
+                            className='form-control mb-4'
+                            value={_idDem} 
+                            onChange={(e) => set_idDem(e.target.value)}
+                        >
+                            <option value="" disabled>Sélectionnez un n° de la demande service</option>
+                            {filteredData.map(item => (
+                                <option key={item._idDem} value={item._idDem}>
+                                    {item._idDem}
+                                </option>
+                            ))}
+                        </select>
+                        <input 
+                            required 
+                            type='text' 
+                            placeholder='Code client' 
+                            className='form-control'
+                            value={clt} 
+                            disabled 
                         />
-                        <input required type='text' placeholder='Designation Machine' className='form-control'
-                            value={Designt} disabled onChange={(e) => { setDesignt(e.target.value) }}
+                        <input 
+                            required 
+                            type='text' 
+                            placeholder='Designation Machine' 
+                            className='form-control'
+                            value={Designt} 
+                            disabled 
                         />
-                        <input required type='text' placeholder='N° Chassis' className='form-control'
-                            value={NSerie} disabled onChange={(e) => { setNSerie(e.target.value) }}
+                        <input 
+                            required 
+                            type='text' 
+                            placeholder='N° Chassis' 
+                            className='form-control'
+                            value={NSerie} 
+                            disabled 
                         />
-                 
                     </div>
                     <div>
                         <h3 className='mx-auto text-center mt-4 my-3 bl'>Pièce et MO réalisé</h3>
-                        <input required type='text' placeholder='Sélectionner les pièces changés' className='form-control mb-4'
-                            value={pieces.length > 0 ? pieces.map(piece => piece.DesignationPmo).join(', ') : ''}
-                            readOnly
-                            onClick={handleShowPopup}
-                        />
-<hr style={{width:'250px',margin:'15px auto'}}></hr>
+                        <div className='d-flex justify-content-center'>
+                            <button className='btn btn-primary mb-2 mt-3' onClick={handleShowPopup}>Ajouter pièce</button>
+                        </div>
+                        <hr style={{width:'250px',margin:'15px auto'}} />
                         <table className="table">  
                             <thead>
                                 <tr>
                                     <th>Article</th>
                                     <th>Designation</th>
                                     <th>Qté</th>
+                                    <th>Actions</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 {pieces.length > 0 ? pieces.map((piece, index) => (
                                     <tr key={index}>
-                                           <td>{piece.Pmo}</td>
+                                        <td>{piece.Pmo}</td>
                                         <td>{piece.DesignationPmo}</td>
                                         <td>{piece.quantity}</td>
+                                        <td>
+                                            <button className='btn btn-danger' onClick={() => handleDeletePiece(index)}>Supprimer</button>
+                                        </td>
                                     </tr>
                                 )) : (
                                     <tr>
-                                        <td colSpan="2">Aucune pièce ajoutée.</td>
+                                        <td colSpan="4">Aucune pièce ajoutée.</td>
                                     </tr>
                                 )}
                             </tbody>
                         </table>
                     </div>
                     {showPopup && <AddPiecePopup onClose={() => setShowPopup(false)} onAddPiece={handleAddPiece} />}
-
-                    <button className='btn btn-dark mt-3 mb-2 col-12 col-md-12'>Valider </button>
-                        <br />
-
+                    <button className='btn btn-dark mt-3 mb-2 col-12 col-md-12'>Valider</button>
                 </div>
             </div>
         </div>
